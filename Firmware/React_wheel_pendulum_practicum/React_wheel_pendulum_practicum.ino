ec46d1faf87187bad2699c379e1ce9de87669522
Aleksandra Fomina <sashavera1998@gmail.com>
2025-06-25 16:21:15 +0300

    modified:   Firmware/React_wheel_pendulum_practicum/React_wheel_pendulum_practicum.ino

--- Firmware/React_wheel_pendulum_practicum/React_wheel_pendulum_practicum.ino
+++ Firmware/React_wheel_pendulum_practicum/React_wheel_pendulum_practicum.ino
@@ -1,22 +1,25 @@
 /*
-The SimpleFoc library is used to control the BLDC motor https://docs.simplefoc.com/ 
-Documentation for the motor driver and VBCore module https://voltbro.gitbook.io/vbcores 
+The SimpleFOC library is used to control the BLDC motor: https://docs.simplefoc.com/ 
+Documentation for the VBCore module: https://voltbro.gitbook.io/vbcores 
+Documentation for the motor driver: https://voltbro.gitbook.io/vbcores/vbcores-hardware/bldc-draiver-30a
 */
+
 #include <VBCoreG4_arduino_system.h>
 #include <Wire.h>
 #include <AS5600.h>
 #include <SimpleFOC.h>
 
-#define pinSDA PB_7_ALT1
-#define pinSCL PC6
-#define pinNSLEEP PB3
+#define PinSDA PB_7_ALT1 // SDA pin of the I2C bus on the motor driver
+#define PinSCL PC6       // SCL pin of the I2C bus on the motor driver
+#define PinNSLEEP PB3    // Pin for enabling/disabling the motor driver
 
-#define K_pa 53.53 //54.9062 
-#define K_pv 7.33 //7.3934
-#define K_ma 0.008 //0.008717 
-#define K_mv 0.023 //0.013016 
-#define K_stand_up 35.0
+#define K_pa 53.53  // Coefficient for pendulum angle (used in stabilization)
+#define K_pv 7.33   // Coefficient for pendulum angular velocity
+#define K_ma 0.008  // Coefficient for motor angle (flywheel)
+#define K_mv 0.023  // Coefficient for motor angular velocity
+#define K_stand_up 35.0 // Coefficient for energy-based swing-up and braking
 
+// Two hardware timers will call control functions periodically
 HardwareTimer *timer_move = new HardwareTimer(TIM7);
 HardwareTimer *timer_foc = new HardwareTimer(TIM2);
 
@@ -27,50 +30,55 @@ float offset;
 float angle_tmp;
 float u = 0;
 
+float k_pa = K_pa; // Coefficient for pendulum angle (state feedback)
+float k_pv = K_pv; // Coefficient for pendulum angular velocity
+float k_ma = K_ma; // Coefficient for motor angle
+float k_mv = K_mv; // Coefficient for motor angular velocity
+float k_balance = K_stand_up; // Coefficient for energy swing-up and braking
 
-float k_pa = K_pa;
-float k_pv = K_pv;
-float k_ma = K_ma;
-float k_mv = K_mv;
-float k_balance = K_stand_up;
+// Physical parameters of the system
+float M = 0.014;      // Mass of the pendulum
+float l = 0.12;       // Length of the pendulum
+float m = 0.063;      // Mass of the motor + flywheel
+float J = M*l*l/3;    // Moment of inertia of the pendulum about its base
+float Jmr = 0.0003385;// Combined moment of inertia of motor and flywheel
+float g = 9.81;       // Gravitational acceleration
 
-float M = 0.014;
-float l = 0.12;
-float m = 0.063;
-float J = M*l*l/3;
-float Jm = 0.0003385;
-float g = 9.81;
-
-float E0 = (M/2+m)*g*l; // energy of the pendulum at the upper equilibrium position
+float E0 = (M/2+m)*g*l; // Desired energy level (upright equilibrium position)
 float E;
-float epsilon = 0.02;
+float epsilon = 0.02;   // Tolerance for switching control modes
 
-float c_vel = 0.5*(J+m*l*l+Jm);
+float c_vel = 0.5*(J+m*l*l+Jmr); // Constant used in energy calculation
 
-SPIClass SPI_3(PC12, PC11, PC10);
+SPIClass SPI_3(PC12, PC11, PC10); // SPI pins on motor driver: PC11 - MISO, PC12 - MOSI, PC10 - SCK
 
-MagneticSensorSPI motor_sensor = MagneticSensorSPI(PA15, 14, 0x3FFF);
-AMS_5600 pendulum_sensor;
+MagneticSensorSPI motor_sensor = MagneticSensorSPI(PA15, 14, 0x3FFF); // Motor sensor over SPI
+AMS_5600 pendulum_sensor; // Pendulum sensor over I2C
 
+// Current sensor: ACS711KEXLT-31AB-T
+// Connected to pins PC1, PC2, PC3 corresponding to I_A, I_B, I_C
 float sensitivity = 45.0;
-InlineCurrentSense current_sense = InlineCurrentSense(sensitivity, PC1, PC2, PC3); 
+InlineCurrentSense current_sense = InlineCurrentSense(sensitivity, PC1, PC2, PC3);  
 
+// Create motor object: 7 pole pairs, 12.2 Ohm resistance, 238 KV
 BLDCMotor motor = BLDCMotor(7, 12.2, 238);
+
+// Create driver object with 3-phase PWM pins
 BLDCDriver3PWM driver = BLDCDriver3PWM(PA8, PA9, PA10);
 
 void setup() {
-  Serial.begin(500000);
-
-  Wire.setSDA(pinSDA);
-  Wire.setSCL(pinSCL);
+  Wire.setSDA(PinSDA);
+  Wire.setSCL(PinSCL);
   Wire.begin();
 
-  pinMode(PB5, INPUT);
-  pinMode(pinNSLEEP, OUTPUT);
-  pinMode(LED2, OUTPUT);
-  digitalWrite(pinNSLEEP, HIGH);
-  pinMode(USR_BTN, INPUT_PULLUP);
+  pinMode(PB5, INPUT);         // NFAULT pin on the driver
+  pinMode(PinNSLEEP, OUTPUT);  // nSLEEP pin: driver enable
+  pinMode(LED2, OUTPUT);       // User LED pin
+  digitalWrite(PinNSLEEP, HIGH); // Enable the driver
+  pinMode(USR_BTN, INPUT_PULLUP); // User button (active LOW)
 
+  // Configure low-side control pins (INLA, INLB, INLC): PB13, PB14, PB15
+  // Enable all low-side switches before driver init
   pinMode(PB15, OUTPUT);
   pinMode(PB14, OUTPUT);
   pinMode(PB13, OUTPUT);
@@ -78,20 +86,31 @@ void setup() {
   digitalWrite(PB14, HIGH);
   digitalWrite(PB13, HIGH);
 
+  // Initialize motor driver using SimpleFOC
   driver.voltage_power_supply = 16;
   driver.init();
   driver.enable();
   motor.linkDriver(&driver);
 
+  // Initialize current sensing using SimpleFOC
   current_sense.init();
   current_sense.linkDriver(&driver);
   motor.linkCurrentSense(&current_sense);
 
+  // Motor setup with SimpleFOC
   motor_sensor.init(&SPI_3);
   motor.linkSensor(&motor_sensor);
-  motor.torque_controller = TorqueControlType::voltage;
+  motor.torque_controller = TorqueControlType::voltage; 
   motor.controller = MotionControlType::torque;
 
+  motor.current_limit = 5;
+  motor.voltage_limit = 16;
+  motor.velocity_limit = 500; 
+  motor.init();
+  motor.initFOC();
+  motor.target = 0;
+
+  // Check pendulum sensor connection
   if(pendulum_sensor.detectMagnet() == 0 ){
     while(1){
         if(pendulum_sensor.detectMagnet() == 1 ){
@@ -105,13 +124,6 @@ void setup() {
         delay(1000);
     }
   }
-  
-  motor.current_limit = 5;
-  motor.voltage_limit = 16;
-  motor.velocity_limit = 500; 
-  motor.init();
-  motor.initFOC();
-
 
   offset = pendulum_sensor.getRawAngle();
   pendulum_angle = pendulum_sensor.getRawAngle() - offset;
@@ -119,62 +131,69 @@ void setup() {
  
   timer_move->pause();
   timer_move->setOverflow(2000, HERTZ_FORMAT); 
-  timer_move->attachInterrupt(move); // the move function will be called by a timer with a frequency of 2kHz
+  timer_move->attachInterrupt(move); // Called at 2kHz for motion control
   timer_move->refresh();
   timer_move->resume();
 
   timer_foc->pause();
   timer_foc->setOverflow(5000, HERTZ_FORMAT);
-  timer_foc->attachInterrupt(FOC_func); // FOC function will be called by a timer with a frequency of 5kHz
+  timer_foc->attachInterrupt(FOC_func); // Called at 5kHz for low-level torque control
   timer_foc->refresh();
   timer_foc->resume();
 
   delay(1000);
-
 }
 
-
+// Function to calculate control voltage for the motor
 void control(){
-  calc_angle_vel(); // angles and velocities from the pendulum arm and the flywheel (motor) are calculated
-  E = c_vel*sq(pendulum_vel)+ E0*cos(pendulum_angle); // pendulum energy
+  calc_angle_vel(); // Get angles and velocities from sensors
+  E = c_vel*sq(pendulum_vel)+ E0*cos(pendulum_angle); // Compute pendulum energy
 
-  if (swing_up_flag){
-    if (abs(E - E0) < epsilon) { // if the energy difference is less than epsilon, we hold the unstable equilibrium state
-      u = -(k_pa*pendulum_angle + k_pv*pendulum_vel + k_ma*motor_angle + k_mv*motor_velocity);   
+  if (swing_up_flag){ // If user activated the pendulum
+    if (abs(E - E0) < epsilon) {
+      // Stabilization around unstable equilibrium
+      u = -(k_pa*pendulum_angle + k_pv*pendulum_vel + k_ma*motor_angle + k_mv*motor_velocity); 
     }
-    else { // else we swing the pendulum
-      u = -k_balance*(E-E0)*sign(pendulum_vel);
+    else {
+      // Energy-based swing-up control
+      u = -k_balance*(E-E0)*sign(pendulum_vel); 
     }
   }
-  else if(stop_flag) u = -k_balance*(E+E0)*sign(pendulum_vel); //u = 0;
+  else if(stop_flag) {
+    // Braking mode
+    u = -k_balance*(E+E0)*sign(pendulum_vel);
+  }
 
-  if (u > 12) u = 12; // limit the voltage to 12 volts
+  // Limit control voltage
+  if (u > 12) u = 12;
   else if (u < -12) u = -12;
   
-  motor.target = u;
+  motor.target = u; // Apply control voltage to the motor
 }
 
-
+// Function to compute pendulum and motor angles and velocities
 void calc_angle_vel(){
-  angle_tmp = pendulum_sensor.getRawAngle()-offset; //read the angle of the pendulum from the sensor
-  angle_tmp = 2048 - angle_tmp; // we read angles from 0 to 4095, let's translate this value to a range of -2048 to 2048
-  pendulum_angle = (PI*angle_tmp)/2048; // translate the raw angle to a range of -pi to pi
+  angle_tmp = pendulum_sensor.getRawAngle()-offset;
+  angle_tmp = 2048 - angle_tmp;
+  pendulum_angle = (PI*angle_tmp)/2048; // Convert to range [-π, π]
   if (pendulum_angle > PI){
     angle_tmp = fmod(pendulum_angle, PI);
     pendulum_angle = angle_tmp - PI;
   }
-  pendulum_vel = (pendulum_angle - prev_angle)*1000; //the function calc_angle_vel is called at a frequency of ~1kHz, so for the speed to be in rad/sec multiply by 1000.
+  pendulum_vel = (pendulum_angle - prev_angle)*1000; // Angular velocity (rad/s), called at ~1kHz
   prev_angle = pendulum_angle;
-  motor_velocity =  -motor.shaft_velocity;
-  motor_angle = -motor.shaft_angle;
+  motor_velocity =  -motor.shaft_velocity; // Motor angular velocity via SimpleFOC
+  motor_angle = -motor.shaft_angle;        // Motor angle via SimpleFOC
 }
 
+// Return sign of input value (-1, 0, or 1)
 int sign(float val){
   if (val < 0) return -1;
   if (val==0) return 0;
   return 1;
 }
 
+// Stop the pendulum: disable control, set voltage to 0
 void stop(){
   u = 0;
   stop_flag = 1;
@@ -183,15 +202,19 @@ void stop(){
   delay(1000);
 }
 
+// Start the pendulum: enable swing-up control
 void start(){
   stop_flag = 0;
   swing_up_flag = 1;
   delay(1000);
 }
 
+// Main loop: handles button state and calls control logic
+// LED ON: pendulum is active and trying to balance
+// LED OFF: pendulum is stopped or braking
 void loop() {
   int buttonState = digitalRead(USR_BTN);
-  if(buttonState == 0){//when the button is pressed, the pendulum starts swinging, if the button is pressed again, the control is switched off
+  if(buttonState == 0){ // Button pressed: toggle start/stop
     if(swing_up_flag == 1){
       digitalWrite(LED2, LOW);
       stop();
@@ -205,10 +228,12 @@ void loop() {
   delay(1);
 }
 
+// Function running low-level torque control loop (5kHz)
 void FOC_func(){
   motor.loopFOC();
 }
+
+// Function running motion control (2kHz), calls motor.move() using motor.target
 void move(){
   motor.move(); 
 }
-
